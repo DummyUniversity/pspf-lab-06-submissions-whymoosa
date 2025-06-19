cf7f1a0c4d684668c7b117b99b923f30460211d5
stmbsnaasmf <43440784+stmbsnaasmf@users.noreply.github.com>
2025-06-19 09:20:33 +0500

    Update student_code.h

--- student_code.h
+++ student_code.h
@@ -1,1 +1,10 @@
-int factorial(int);
+#include <stdbool.h>
+
+int autograde(char*);
+int testFactorial(int);
+
+int testPower(int n, int p);
+bool testIsPrime(int n);
+int testNthFibonacciTerm (int n);
+bool testIsMultiple(int n, int m);
+int testSeriesSum(int n);

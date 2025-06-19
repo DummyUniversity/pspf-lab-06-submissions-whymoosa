2ff2f1d249cabf7b85ea0cc6ac0f7c9c06b57691
stmbsnaasmf <43440784+stmbsnaasmf@users.noreply.github.com>
2025-06-19 09:22:42 +0500

    Update student_code.c

--- student_code.c
+++ student_code.c
@@ -4,19 +4,43 @@ int factorial(int n)
 {
 	int result = 0;
 	
-//	if (0 == n || 1 == n)
-//	{
-//		result = 1;
-//	}
-//	else
-//	{
-//		result = n;
-//		
-//		for (int i = n - 1; i > 0; i--)
-//		{
-//			result = result * i; 
-//		}
-//	}
+	if (0 == n || 1 == n)
+	{
+		result = 1;
+	}
+	else
+	{
+		result = n;
+		
+		for (int i = n - 1; i > 0; i--)
+		{
+			result = result * i; 
+		}
+	}
 
 	return result;
+}
+
+int power(int n, int p)
+{
+	return 0;
+}
+bool isPrime(int n)
+{
+	return 0;
+}
+
+int nthFibonacciTerm (int n)
+{
+	return 0;
+}
+
+bool isMultiple(int n, int m)
+{
+	return 0;
+}
+
+int seriesSum(int n)
+{
+	return 0;
 }
